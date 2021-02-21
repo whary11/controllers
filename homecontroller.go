7a80daf9b567726505b4c32c0c680b63a4d2b35c
Luis Fernando Raga <luis.raga@laika.com.co>
2021-02-21 15:11:32 -0500

    controllers init

---
+++ homecontroller.go
@@ -0,0 +1,14 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HomeHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+		"name":    "Luis Fernando Raga",
+	})
+}
